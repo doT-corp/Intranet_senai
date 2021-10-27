5264dc271f356c17cc693efe69a387ee5f403c09
Diegusto <diegusto35@gmail.com>
2021-10-27 15:26:18 -0300

    intranet nova

---
+++ java/script.js
@@ -0,0 +1,299 @@
+$(document).ready(function() {
+    var botao = $('.bt1');
+    var dropDown = $('.ul1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+$(document).ready(function() {
+    var botao = $('.bt2');
+    var dropDown = $('.ul2');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt3');
+    var dropDown = $('.ul3');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt3_1');
+    var dropDown = $('.ul3_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt3_2');
+    var dropDown = $('.ul3_2');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt4');
+    var dropDown = $('.ul4');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt5');
+    var dropDown = $('.ul5');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt6');
+    var dropDown = $('.ul6');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt7');
+    var dropDown = $('.ul7');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt8');
+    var dropDown = $('.ul8');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt8_1');
+    var dropDown = $('.ul8_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt8_2');
+    var dropDown = $('.ul8_2');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt9');
+    var dropDown = $('.ul9');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt10');
+    var dropDown = $('.ul10');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt11');
+    var dropDown = $('.ul11');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt11_1');
+    var dropDown = $('.ul11_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt11_2');
+    var dropDown = $('.ul11_2');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt12');
+    var dropDown = $('.ul12');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt13');
+    var dropDown = $('.ul13');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt13_1');
+    var dropDown = $('.ul13_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt14');
+    var dropDown = $('.ul14');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt15');
+    var dropDown = $('.ul15');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt15_1');
+    var dropDown = $('.ul15_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt15_2');
+    var dropDown = $('.ul15_2');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt16');
+    var dropDown = $('.ul16');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt17');
+    var dropDown = $('.ul17');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt17_1');
+    var dropDown = $('.ul17_1');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt18');
+    var dropDown = $('.ul18');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt19');
+    var dropDown = $('.ul19');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
+
+   $(document).ready(function() {
+    var botao = $('.bt0');
+    var dropDown = $('.n0');    
+   
+       botao.on('click', function(event){
+           dropDown.stop(true,true).slideToggle();
+           event.stopPropagation();
+       });
+   });
